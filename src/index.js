2aeaac844bdbda6d27350d4e76561e4978bc41c9
Applopie <stina.nekrasova.2@yandex.ru>
2026-03-17 18:40:09 +0500

    added Thrasher

--- src/index.js
+++ src/index.js
@@ -54,25 +54,46 @@ class Duck extends Creature {
     };
 }
 
-
 // Основа для собаки.
 class Dog extends Creature {
-    constructor() {
-        super('Пес-Бандит', 3);
+    constructor(name = 'Пес-Бандит', power = 3) {
+        super(name, power);
     }
 }
 
+class Trasher extends Dog {
+    constructor() {
+        super('Громила', 5);
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        if (value <= 0) {
+            continuation(value);
+            return;
+        }
+
+        const newValue = Math.max(value - 1, 0);
+        this.view.signalAbility(() => {
+            continuation(newValue);
+        })
+    }
+
+    getDescriptions() {
+        const descriptions = super.getDescriptions();
+        descriptions.push('Получает на 1 меньше урона');
+        return descriptions;
+    }
+}
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
+    new Duck(),
 ];
-
-// Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Dog(),
+    new Trasher(),
 ];
 
 
